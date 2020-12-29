e4de48de39ad467d8715629909f962114d113b4a
JosiahGodwin <josiahgodwin2@gmail.com>
2020-12-29 17:55:49 +0000

    Walking animation + basic shooting
    Walking and idle now have animations, as well as a new script for shooting and for the targets

---
+++ Solo_FPS/Assets/Scripts/AK_Animator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AK_Animator : MonoBehaviour
+{
+    public Animator anim;
+
+    void Update()
+    {
+        anim.SetBool("Vertical", Input.GetButton("Vertical"));
+        anim.SetBool("Horizontal", Input.GetButton("Horizontal"));
+    }
+}
