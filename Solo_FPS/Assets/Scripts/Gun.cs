73e920a2e2c9a6c673493b36a6ad0fd9078f1948
JosiahGodwin <josiahgodwin2@gmail.com>
2020-12-30 21:16:39 +0000

    Sparks added when shooting something
    New particle system and code for sparks when shooting objects. There has also been a sound for the AK47 added, but it is currently only in the folder

--- Solo_FPS/Assets/Scripts/Gun.cs
+++ Solo_FPS/Assets/Scripts/Gun.cs
@@ -4,9 +4,13 @@ public class Gun : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+    public float fireRate = 15f;
 
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+
+    private float nextTimeToFire = 0f;
 
     void Update()
     {
@@ -23,13 +27,14 @@ public class Gun : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
-            Debug.Log(hit.transform.name);
-
             Target target = hit.transform.GetComponent<Target>();
             if (target != null)
             {
                 target.TakeDamage(damage);
             }
+
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f);
         }
     }
 }
